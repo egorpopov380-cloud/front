b9be50318e3aba0109b216200617b993831ea3ec
egorpopov380-cloud <egorpopov380@gmail.com>
2026-05-14 14:17:30 +0400

    Update request.js

--- js/request.js
+++ js/request.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3000'
+const API = 'https://kval.onrender.com'
 
 async function createNewRequest() {
     const selectedCourse = document.getElementById('kursName').value;
